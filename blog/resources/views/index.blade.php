cb4e1ce7c34fdde7237b4439a4ea46f5dd477a8b
sulemankhan447 <sulemankhan447@gmail.com>
2018-09-11 16:14:28 +0530

    laravel

---
+++ blog/resources/views/index.blade.php
@@ -0,0 +1,5 @@
+@extends('template')
+@section('title','Home Page')
+@section('content')
+    <h1>Welcome TO Laravel Home page</h1>
+   @endsection  
---
+++ blog/resources/views/template.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>@yield('title')</title>
+        <link rel="stylesheet" type="text/css" href="{{asset('css/styles.css')}}">
+
+       
+    </head>
+    <body>
+        @yield('content')
+        <script  src="{{asset('js/app.js')}}" type="text/javascript"></script>
+        <script  src="{{asset('js/bootstrap.js')}}" type="text/javascript"></script>
+
+        @yield('scripts')
+
+    </body>
+</html>
