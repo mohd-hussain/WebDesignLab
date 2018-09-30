ae6ff5c0526e9549bcd91e2bebe2cf2da2077ad4
hussain <root@hussain.com>
2018-09-30 23:18:47 +0530

    laravel

--- blog/resources/views/gallery.blade.php
+++ blog/resources/views/gallery.blade.php
@@ -1,51 +1,48 @@
 @extends('template')
-@section('title','resume')
+@section('title','My Resume')
 
 @section('navbar')
     @include('partials.navbar')
 @endsection
 
 @section('content')
-    <h1>Gallery</h1>
+   <div class="gallery col-lg-12">
+			<h1>Gallery</h1>
+			<div class="row">
+				<figure class="col-lg-3 col-md-5 col-sm-12">
+					<img src="{{asset('img/beach.jpg')}}" alt="Cactus Plant">
+					<figcaption>A cactus plant</figcaption>
+				</figure>
 
-      <main>
-		<div class="gallery">
-			<h1>Gallery </h1>
-		<figure>
-			<img src="{{asset('img/cactus.jpg')}}" alt="Cactus Plant">
-			<figcaption>A cactus plant</figcaption>
-		</figure>
+				<figure class="col-lg-3 col-md-5 col-sm-12">
+					<img src="{{asset('img/arrangement.jpg')}}" alt="A flower">
+					<figcaption>Flowers</figcaption>
+				</figure>
 
-		<figure>
-			<img src="{{asset('img/arrangement.jpg')}}" alt="A flower">
-			<figcaption>Flowers</figcaption>
-		</figure>
+				<figure class="col-lg-3 col-md-5 col-sm-12">
+					<img src="{{asset('img/beach.jpg')}}" alt="A beach">
+					<figcaption>A beautiful beach</figcaption>
+				</figure>
+				
+			</div>
 
-		<figure>
-			<img src="{{asset('img/beach.jpg')}}" alt="A beach">
-			<figcaption>A beautiful beach</figcaption>
-		</figure>
+			<figure class="col-lg-3 col-md-5 col-sm-12">
+				<img src="{{asset('img/clock.svg')}}" alt="A Clock">
+				<figcaption>A Clock</figcaption>
+			</figure>
 
-		<figure>
-			<img src="{{asset('img/clock.svg')}}" alt="A Clock">
-			<figcaption>A Clock</figcaption>
-		</figure>
+			<figure class="col-lg-3 col-md-5 col-sm-12">
+				<img src="{{asset('img/mobile-phone.jpg')}}" alt="A mobile phone">
+				<figcaption>A Mobile Phone</figcaption>
+			</figure>
 
-		<figure>
-			<img src="{{asset('img/mobile-phone.jpg')}}" alt="A mobile phone">
-			<figcaption>A Mobile Phone</figcaption>
-		</figure>
-
-		<figure>
-			<img src="{{asset('img/mobile.jpg')}}" alt="A Mobile">
-			<figcaption>Another Mobile</figcaption>
-		</figure>	
+			<figure class="col-lg-3 col-md-5 col-sm-12">
+				<img src="{{asset('img/mobile-phone.jpg')}}" alt="A Mobile">
+				<figcaption>Another Mobile</figcaption>
+			</figure>
 		</div>
-		
-	</main>
-
 @endsection
 
 @section('footer')
-    @include('partials.footer')    
-@endsection
+	@include('partials.footer')
+@endsection	
--- blog/resources/views/index.blade.php
+++ blog/resources/views/index.blade.php
@@ -6,21 +6,26 @@
 @endsection
 
 @section('content')
-    <h1>Welcome to laravel</h1>
+    
+   <article class="container panel panel-default">
+		<div class="panel-heading">Hello World!</div>
+		<header class="panel-body">
+			<div>
+				<span>Posted on</span> 
+				<a href="#">June 20, 2008</a>
+			</div>
+			<h3 class="entry-title">
+				<a href="#">Hello World</a>
+			</h3>	
+		</header>
 
-    <div>
-    	<p>
-    		Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+	
+		<div class="panel-body">
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis veritatis soluta nisi, molestias explicabo dolorum iure nesciunt porro esse! Eveniet, magni corporis unde excepturi dolorum, veritatis! Possimus quidem totam architecto! </p>
+		</div>
 
-
-
-
-           <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..",</p>
-
-
-
-    	
-    </div>
+	
+</article>
 
 @endsection
 
